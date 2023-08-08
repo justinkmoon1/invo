7c57a13bada7c955d9be33cbd2e903c9e91ba205
justinkmoon1 <87982180+justinkmoon1@users.noreply.github.com>
2023-08-08 18:46:25 +0900

    data query complete

--- tools/data_query.py
+++ tools/data_query.py
@@ -2,12 +2,12 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('C:/Users/Justin Moon/Dropbox/청심국제고 2학년 기록/개인생활/대회/Wharton Investment Competition/invo/data/raw/company_list.csv')
-stock_list = data['Symbol'].tolist()
+data = pd.read_csv("data/raw/company_list.csv")
+stock_list = data['Symbol'].tolist()[:400]
+stock_data = yf.download(stock_list, start='2010-01-01', group_by='Ticker')
+file_path = 'data/raw/stock_data.csv'
+stock_data.to_csv(file_path)
 
-
-
-print(stock_list)
 
 
 
