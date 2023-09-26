75dfcf532ab013b0ff05f2b55d02dad1700c2edf
justinkmoon1 <87982180+justinkmoon1@users.noreply.github.com>
2023-09-27 07:14:10 +0900

    model structure for now

--- models/LSTM/model/lstm_utils.py
+++ models/LSTM/model/lstm_utils.py
@@ -13,7 +13,7 @@ def create_dataset(dataset, look_back=1):
     for i in range(len(dataset) - look_back - 1):
         a = dataset[i:(i+look_back), 0]
         dataX.append(a)
-        dataY.append(dataset[i + look_back], 0)
+        dataY.append(dataset[i + look_back, 0])
         return np.array(dataX), np.array(dataY)
 
     
--- models/LSTM/model/main.py
+++ models/LSTM/model/main.py
@@ -1,5 +1,5 @@
-#LSTM for regression
-import numpy as np
+#LSTM with window method
+import numpy
 import matplotlib.pyplot as plt
 from pandas import read_csv
 import math
@@ -8,52 +8,66 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
-from lstm_utils import *
-from model import StockPredictionLSTM
-
-np.random.seed(7)
-
-DATA_PATH = ""
-
-dataframe = read_csv(DATA_PATH, usecols=[1], engine = 'python')
+DATA_PATH = 'models/LSTM/data/test_data.csv'
+#convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + look_back, 0])
+    return numpy.array(dataX), numpy.array(dataY)
+#fix random seed for reproducibility
+numpy.random.seed(7)
+#load the dataset
+dataframe = read_csv(DATA_PATH, usecols=[1], engine='python')
 dataset = dataframe.values
 dataset = dataset.astype('float32')
-
+#normalize the dataset
 scaler = MinMaxScaler(feature_range=(0, 1))
 dataset = scaler.fit_transform(dataset)
-
-train_size = int(len(dataset) * 0.67)
+#split into train and test sets
+train_size = int(len(dataset) * 0.9)
 test_size = len(dataset) - train_size
 train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-
-look_back = 1
+'''*************************************'''
+#reshape into X=t and Y=t+1
+look_back = 90 #window-method (t-2,t-1,t,y)
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
-
-model = StockPredictionLSTM(look_back)
-model.train(trainX, trainY, epochs=100, batch_size=28, verbose=1)
-
+#reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+'''**************************************'''
+#create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(17, input_shape=(1, look_back)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam',metrics=["accuracy"])
+model.fit(trainX, trainY, epochs=450, batch_size=28, verbose=1)
+#make predictions
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
-
-trainPredict = scaler.inverse_transform(testPredict)
+#invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
 testY = scaler.inverse_transform([testY])
-
-trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
+#calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
-
-testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
-
-
-trainPredictPlot = np.empty_like(dataset)
-trainPredictPlot[:, :] = np.nan
-trainPredictPlot[look_back: len(trainPredict) + look_back, :] = trainPredict
-
-testPredictPlot = np.empty_like(dataset)
-testPredictPlot[:, :] = np.nan
-testPredictPlot[len(trainPredict) + look_back*2 + 1:len(dataset) - 1, :] = testPredict
-
+#-----------------Visualize----------------
+#shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+#shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
 plt.plot(scaler.inverse_transform(dataset))
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
