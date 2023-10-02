448aa6f7cccf6f47430b93eaf80b1e6e7e4a8907
justinkmoon1 <87982180+justinkmoon1@users.noreply.github.com>
2023-10-03 04:47:53 +0900

    Screening Final

--- tools/screening.py
+++ tools/screening.py
@@ -1,16 +1,21 @@
 import pandas as pd
 from openpyxl import load_workbook
 
-DATA_PATH = 'data/raw/Stock List-FINAL.xlsx'
-EVTOEBIT_PATH = 'data/processed/evtoebit.xlsx'
-RESULT_PATH = 'data/processed/after_screening.xlsx'
+#PATH Settings
+DATA_PATH = 'data/raw/Stock List.xlsx'
+EVTOEBIT_PATH = 'data/processed/evtoebit_final.xlsx'
+RESULT_PATH = 'data/processed/after_screening_final.xlsx'
 AVG_PATH = 'data/processed/evtoebit_avgs.xlsx'
 INDLIST_PATH = 'data/processed/industries.xlsx'
+
+#Stats Loading
 indlist = pd.read_excel(INDLIST_PATH)
 evtoebits = pd.read_excel(EVTOEBIT_PATH)
 evtoebitavg = pd.read_excel(AVG_PATH)
-evtoebits.set_index('Ticker')
 stock_list = pd.ExcelFile(DATA_PATH)
+evtoebits.set_index('Ticker')
+
+#Sheets Loading
 financials_sheet = pd.read_excel(stock_list, 'Financials')
 consumer_discretionary_sheet = pd.read_excel(stock_list, 'Consumer Discretionary')
 industrials_sheet = pd.read_excel(stock_list, 'Industrials')
@@ -18,20 +23,23 @@ healthcare_sheet = pd.read_excel(stock_list, 'Healthcare')
 energy_sheet = pd.read_excel(stock_list, 'Energy')
 utilities_sheet = pd.read_excel(stock_list, 'Utilities')
 excel_book = load_workbook(RESULT_PATH)
-# financials_avg = 
-# consumer_discretionary_avg = 
+
+
+
 
 def screening(sheet, new_sheet_name):
+    errors = 0
     results = {}
     evtoebits_final = {}
     evtoebits_avg = {}
-    for company_name in sheet['Ticker']:
+    for i in range(sheet.shape[0]):
+        company_name = sheet.iloc[i]['Ticker']
         try:
-            evtoebit = evtoebits['EVtoEBIT'].iloc[evtoebits[evtoebits['Ticker'] == company_name].index]
+            evtoebit = evtoebits['EVtoEBIT'].iloc[evtoebits[evtoebits['Ticker'] == sheet.iloc[i]['Ticker']].index]
             if len(evtoebit) == 0:
                 raise IndexError
-            industry = indlist[company_name].values[0]
-            avgevtoebit = float(evtoebitavg.loc[evtoebitavg['Industry Name'] == industry]['EV/EBIT'])
+            industry = indlist[sheet.iloc[i]['Original']]
+            avgevtoebit = float(evtoebitavg.loc[evtoebitavg['Industry Name'] == industry.values[0]]['EV/EBIT'])
             if evtoebit.values > avgevtoebit:
                 results[company_name] = 1
                 evtoebits_final[company_name] = evtoebit.values
@@ -41,6 +49,7 @@ def screening(sheet, new_sheet_name):
                 evtoebits_final[company_name] = evtoebit.values
                 evtoebits_avg[company_name] = avgevtoebit
         except:
+            errors += 1
             results[company_name] = -1
             evtoebits_final[company_name] = "N/A"
             evtoebits_avg[company_name] = "N/A"
@@ -61,13 +70,13 @@ def screening(sheet, new_sheet_name):
     final3 = pd.concat([final2, evtoebits_avg_df['EV/EBIT AVG']], axis=1)
     with pd.ExcelWriter(RESULT_PATH, engine = 'openpyxl', mode='a') as writer:
         final3.to_excel(writer, sheet_name = new_sheet_name)
+    print(errors)
 
+
+#Call Functions
 screening(financials_sheet, 'Financials')
 screening(consumer_discretionary_sheet, 'Consumer Discretionary')
 screening(industrials_sheet, 'Industrials')
 screening(healthcare_sheet, 'Healthcare')
 screening(energy_sheet, 'Energy')
 screening(utilities_sheet, 'Utilities')
-
-        
-
